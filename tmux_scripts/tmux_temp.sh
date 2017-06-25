aba10889a5e8742f5c0672de1516cbc2be6d7b26
Thomas Pilgaard Nielsen <tpilgaards@gmail.com>
2017-06-25 17:16:08 +0200

    Port dotfiles to laptop

--- tmux_scripts/tmux_temp.sh
+++ tmux_scripts/tmux_temp.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-# echo " $(/opt/vc/bin/vcgencmd measure_temp | grep -o '[0-9.]\+') C"
-temp=$(/opt/vc/bin/vcgencmd measure_temp)
-temp=${temp:5:4}
-echo -e " $temp\u00B0C"
+raw="$(sensors)"
+regex_cpu="k10temp-pci-00c3\s+Adapter:\sPCI\sadapter\s+temp1:\s+\+([[:digit:]]+.[[:digit:]].[[:upper:]])"
+if [[ $raw =~ $regex_cpu ]]; then
+	temp_cpu="${BASH_REMATCH[1]}"
+fi
+
+regex_gpu="radeon-pci-0008\s+Adapter:\sPCI\sadapter\s+temp1:\s+\+([[:digit:]]+.[[:digit:]].[[:upper:]])"
+if [[ $raw =~ $regex_gpu ]]; then
+	temp_gpu="${BASH_REMATCH[1]}"
+fi
+
+echo " CPU: $temp_cpu GPU: $temp_gpu"
